957b59641ff27e78dca50ff3ca8867cbb3b72529
Rakha Putra Pratama <rakhacimano@gmail.com>
2022-02-11 10:24:37 +0700

    Sukses Menampilkan Data Member

---
+++ src/components/App.vue
@@ -0,0 +1,290 @@
+<template>
+  <div>
+    <div id="wrapper">
+      <ul
+        class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+        id="accordionSidebar"
+      >
+        <a
+          class="sidebar-brand d-flex align-items-center justify-content-center"
+          href="index.html"
+        >
+          <div class="sidebar-brand-icon">
+            <img src="./src/assets/img/Favicon.svg" alt="Logo Londri" />
+          </div>
+          <h4
+            class="sidebar-brand-text ml-2 mt-2 font-weight-bold text-capitalize"
+          >
+            Londri
+          </h4>
+        </a>
+        <hr class="sidebar-divider my-0" />
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+          </router-link>
+        </li>
+        <hr class="sidebar-divider" />
+        <div class="sidebar-heading">Interface</div>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/member">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Member</span>
+          </router-link>
+        </li>
+      </ul>
+      <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+          <nav
+            class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow"
+          >
+            <button
+              id="sidebarToggleTop"
+              class="btn btn-link d-md-none rounded-circle mr-3"
+            >
+              <i class="fa fa-bars"></i>
+            </button>
+            <form
+              class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search"
+            >
+              <div class="input-group">
+                <input
+                  type="text"
+                  class="form-control bg-light border-0 small"
+                  placeholder="Search for..."
+                  aria-label="Search"
+                  aria-describedby="basic-addon2"
+                />
+                <div class="input-group-append">
+                  <button class="btn btn-primary" type="button">
+                    <i class="fas fa-search fa-sm"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item dropdown no-arrow d-sm-none">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="searchDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <i class="fas fa-search fa-fw"></i>
+                </a>
+                <div
+                  class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                  aria-labelledby="searchDropdown"
+                >
+                  <form class="form-inline mr-auto w-100 navbar-search">
+                    <div class="input-group">
+                      <input
+                        type="text"
+                        class="form-control bg-light border-0 small"
+                        placeholder="Search for..."
+                        aria-label="Search"
+                        aria-describedby="basic-addon2"
+                      />
+                      <div class="input-group-append">
+                        <button class="btn btn-primary" type="button">
+                          <i class="fas fa-search fa-sm"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </li>
+
+              <li class="nav-item dropdown no-arrow mx-1">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="messagesDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <i class="fas fa-envelope fa-fw"></i>
+                  <span class="badge badge-danger badge-counter">7</span>
+                </a>
+                <div
+                  class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                  aria-labelledby="messagesDropdown"
+                >
+                  <h6 class="dropdown-header">Message Center</h6>
+                  <a class="dropdown-item d-flex align-items-center" href="#">
+                    <div class="dropdown-list-image mr-3">
+                      <img
+                        class="rounded-circle"
+                        src="https://source.unsplash.com/fn_BT9fwg_E/60x60"
+                        alt=""
+                      />
+                      <div class="status-indicator bg-success"></div>
+                    </div>
+                    <div class="font-weight-bold">
+                      <div class="text-truncate">
+                        Hi there! I am wondering if you can help me with a
+                        problem I've been having.
+                      </div>
+                      <div class="small text-gray-500">Emily Fowler · 58m</div>
+                    </div>
+                  </a>
+                  <a
+                    class="dropdown-item text-center small text-gray-500"
+                    href="#"
+                    >Read More Messages</a
+                  >
+                </div>
+              </li>
+              <div class="topbar-divider d-none d-sm-block"></div>
+
+              <li class="nav-item dropdown no-arrow">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="userDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <span
+                    class="mr-2 d-none d-lg-inline text-capitalize text-gray-600 small font-weight-bold"
+                    >{{ username }}</span
+                  >
+                  <img
+                    class="img-profile rounded"
+                    src="https://source.unsplash.com/WNoLnJo7tS8/60x60"
+                  />
+                </a>
+                <div
+                  class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                  aria-labelledby="userDropdown"
+                >
+                  <a class="dropdown-item" href="#">
+                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Profile
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Settings
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Activity Log
+                  </a>
+                  <div class="dropdown-divider"></div>
+                  <a
+                    class="dropdown-item"
+                    href="#"
+                    data-toggle="modal"
+                    data-target="#logoutModal"
+                  >
+                    <i
+                      class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"
+                    ></i>
+                    Logout
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </nav>
+
+          <div class="container-fluid">
+            <div class="mb-4">
+              <router-view></router-view>
+            </div>
+          </div>
+        </div>
+        <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright &copy; Londri 2022</span>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <div
+      class="modal fade"
+      id="logoutModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button
+              class="close"
+              type="button"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            Select "Logout" below if you are ready to end your current session.
+          </div>
+          <div class="modal-footer">
+            <button
+              class="btn btn-secondary"
+              type="button"
+              data-dismiss="modal"
+            >
+              Cancel
+            </button>
+            <a class="btn btn-primary" @click="Logout" href="#">Logout</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+module.exports = {
+  data: function () {
+    return {
+      username: "",
+      role: "",
+    };
+  },
+  methods: {
+    getInfo: function () {
+      let config = {
+        headers: {
+          Authorization: "Bearer " + this.$cookies.get("Authorization"),
+        },
+      };
+
+      axios.get(base_url + "/user/login/check", config).then((response) => {
+        if (response.data.success == true) {
+          this.username = response.data.data.username;
+          this.role = response.data.data.role;
+        }
+      });
+    },
+    Logout: function () {
+      this.$cookies.remove("Authorization");
+      this.componentName = "login";
+
+      window.location == front_url;
+    },
+  },
+  mounted() {
+    this.getInfo();
+  },
+};
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-xl-9 col-lg-12 col-md-9">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="">
+                    <h1 class="h4 text-gray-900">Oops. Kami Melupakanmu :(</h1>
+                    <p class="mb-4">
+                      Login kembali untuk dapat mengakses Londri!
+                    </p>
+                  </div>
+                  <form class="user" @submit.prevent="Login" method="post">
+                    <div class="form-group">
+                      <input
+                        v-model="username"
+                        type="text"
+                        class="form-control form-control-user"
+                        id="exampleInputUsername"
+                        aria-describedby="usernameHelp"
+                        placeholder="Masukkan Username"
+                      />
+                    </div>
+                    <div class="form-group">
+                      <input
+                        v-model="password"
+                        type="password"
+                        class="form-control form-control-user"
+                        id="exampleInputPassword"
+                        placeholder="Masukkan Password"
+                      />
+                    </div>
+                    <button
+                      class="btn btn-primary btn-user btn-block font-weight-bold"
+                    >
+                      Login
+                    </button>
+                  </form>
+                  <hr />
+                  <div class="text-center">
+                    <a class="small" href="forgot-password.html"
+                      >Lupa Password?</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+      message: "",
+    };
+  },
+  methods: {
+    Login: function () {
+      this.message = "Mohon tunggu..."; //alert notif
+
+      //mapping data
+      let form = {
+        username: this.username,
+        password: this.password,
+      };
+
+      axios.post(base_url + "/user/login", form).then((response) => {
+        if (response.data.success == true) {
+          this.message = response.data.message; //alert
+
+          //cek apakah token sudah ada di cookies???
+          if (this.$cookies.isKey("Authorization")) {
+            this.$cookies.remove("Authorization");
+          }
+
+          //menyimpan token ke cookies
+          this.$cookies.set("Authorization", response.data.data.token);
+
+          location.reload();
+        } else {
+          this.message = response.data.message;
+        }
+      });
+    },
+  },
+};
+</script>
